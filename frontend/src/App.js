15032fddf0c2222fef6304d5c69a231b9a531e7e
KaanC3tin <kaan_1536@outlook.com>
2024-06-21 20:04:12 +0300

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LoginPage from './components/LoginPage';
+import HomePage from './components/HomePage'
+import Navbar  from './components/Navbar'
+import Signup from './components/Signup';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />  {/* Navbar bileşenini Routes bileşeninin dışına taşıyoruz */}
+      <Routes>
+      <Route path="/" element={<HomePage />} />
+       
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/Signup" element={<Signup />} />
+
+
+      
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
